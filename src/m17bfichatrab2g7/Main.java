2b596754faf7e11f2b73b4ed4ede61cf859a7a8d
ruben <ruben@DESKTOP-IC6A7H4.lan>
2018-02-08 22:15:08 +0000

    versão 1.1

---
+++ src/m17bfichatrab2g7/Main.java
@@ -0,0 +1,16 @@
+/*
+ Traçagem do programa
+ */
+package m17bfichatrab2g7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b, k = 0;
+        for (a = 0; a < 10; a++) {
+            for (b = a; b < 10; b++) {
+                k = a + b;
+            }
+        }
+    }
+}
